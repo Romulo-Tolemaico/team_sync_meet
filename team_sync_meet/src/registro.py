ca07de041f54db884b7c0961c6e8b2c4027954e3
Rodrigo Checo <checorodrigo.cr@gmail.com>
2025-05-11 20:55:08 -0400

    Agregando registro.py desde la rama Rodrigo

---
+++ team_sync_meet/src/registro.py
@@ -0,0 +1,24 @@
+import random
+from colorama import init, Fore
+
+init(autoreset=True)
+
+class Registro:
+    def __init__(self):
+        self.nombre = None
+
+    def generarNombreAutomatico(self):
+        return Fore.CYAN + f"Usuario_{random.randint(100, 999)}"
+
+    def obtenerNombre(self):
+        entrada = input(Fore.GREEN + "🟢 Ingresa tu nombre o pulsa ENTER para generar uno automático: ").strip()
+        if not entrada:
+            self.nombre = self.generarNombreAutomatico()
+        else:
+            self.nombre = entrada
+        print(Fore.CYAN + f"\n👤 > {self.nombre} ingresó al chat")
+        return self.nombre
+
+if __name__ == "__main__":
+    usuario = Registro()
+    usuario.obtenerNombre()
